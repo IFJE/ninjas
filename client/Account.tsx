8d92d97c2821b86764e0a3c74bc2ffd57603fba7
lunaf-github <lunaf91@gmail.com>
2022-04-20 12:21:03 -0700

    Create Account component

---
+++ client/Account.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Inputs from './Inputs';
+import MainContainer from './MainContainer';
+import Form from './Form';
+
+function Account() {
+   // we need conditional rendering between Inputs/MainContainer and From Components
+     
+
+    return (
+        <div id="account">
+         <Inputs/>
+         <MainContainer/>
+         {/* <Form /> */}
+        </div>
+    )
+}
+
+export default Account;
