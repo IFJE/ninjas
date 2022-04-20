f94fc2ebe970a0e9c1c910a089b1a21ca9ba8a30
Joseph Torsella <joseph.r.torsella@gmail.com>
2022-04-19 20:41:47 -0400

    chore: barebones routing for authentication

--- server/routes/apiRouter.ts
+++ server/routes/apiRouter.ts
@@ -1,7 +1,13 @@
-import express, {Express, Request, Response} from 'express';
+import express = require('express');
 
-const router = require('express').Router();
-router.get('/', (req: Request, res: Response) => {
+const { register, login, authenticate } = require('../controllers/authController');
+
+const router = express.Router();
+
+router.post('/register', register, (req : express.Request, res : express.Response) => res.sendStatus(201));
+router.post('/login', login, (req : express.Request, res : express.Response) => res.sendStatus(201));
+
+router.get('/', (req: express.Request, res: express.Response) => {
   console.log('on api path');
   return res.status(200).json();
 });
