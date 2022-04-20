8b8332f951fe64a4e90aa5298dc187d8a11382a8
Joseph Torsella <joseph.r.torsella@gmail.com>
2022-04-20 15:17:09 -0400

    Build routes

--- server/routes/apiRouter.ts
+++ server/routes/apiRouter.ts
@@ -1,16 +1,18 @@
 import express = require('express');
 
 const { register, login, authenticate } = require('../controllers/authController');
-const userInfoController = require('../controllers/userInfoController');
+const { getData } = require('../controllers/userInfoController');
 
 const router = express.Router();
 
 router.post('/register', register, (req : express.Request, res : express.Response) => res.sendStatus(201));
-router.post('/login', login, (req : express.Request, res : express.Response) => res.sendStatus(201));
-
-router.get('/', (req: express.Request, res: express.Response) => {
-  console.log('on api path');
-  return res.status(200).json();
+router.post('/login', login, (req : express.Request, res : express.Response) => {
+  if (res.locals.loggedIn === true) {
+    return res.sendStatus(201);
+  }
+  return res.sendStatus(401);
 });
+
+router.get('/data', authenticate, getData, (req: express.Request, res: express.Response) => res.status(200).json(res.locals.data));
 
 module.exports = router;
