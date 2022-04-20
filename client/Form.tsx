c7591c32620de8b3fda48140440903e1deeb619b
lunaf-github <lunaf91@gmail.com>
2022-04-20 12:21:50 -0700

    Create Form component

---
+++ client/Form.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function Form(){
+  return(
+   <div id="form">
+    <form>
+     <div>
+       <input type="text" />
+     </div>
+     <div>
+       <input type="text" />
+     </div>
+     <div>
+       <input type="text" />
+     </div>
+     <div>
+       <input type="text" />
+     </div>
+     <div>
+       <input type="text" />
+     </div>
+     <div>
+       <input type="text" />
+     </div>
+     <div>
+       <input type="text" />
+     </div>
+     <div>
+       <button>Submit</button>
+       <button>Cancel</button>
+     </div>
+    </form>
+   </div>
+  );
+}
+
+export default Form;
